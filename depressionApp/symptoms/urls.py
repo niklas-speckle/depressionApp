70f82d5a7e5ba5a9a6a58a79883d82b08bf44194
Niklas Speckle <niklas@speckle.at>
2025-04-15 15:02:05 +0200

    added requirements.txt

--- depressionApp/symptoms/urls.py
+++ depressionApp/symptoms/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.dashboard, name = "dashboard"),
     path('symptomAssessment/<int:questionnaire_id>', views.symptomAssessment_view, name="symptomAssessment"),
     path('questionnaireSelection/', views.questionnaireSelection_view, name="questionnaireSelection"),
-    path('patients/questionnaire_score/<slug:profile_slug>/', views.get_questionnaire_scores, name="questionnaire_scores"),
+    path('patients/questionnaire_scores/<slug:profile_slug>/', views.get_questionnaire_scores, name="questionnaire_scores"),
     path('patients/', views.dashboard, name="patients"),
     path('patients/<slug:slug>/', views.user_responses_view, name="user_responses"),
 ]
